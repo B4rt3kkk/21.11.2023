0908a5ce398e3ee1fa615c93a979b78bf753a418
FilipZembrzuski <fzembrzuski@gmail.com>
2023-11-21 10:28:45 +0100

    eeeeluwina

---
+++ Zadanie1/Zadanie1/Zadanie1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string* wsk;
+	string nazwa = "Bartek";
+	cout << "Wskazniki" << endl;
+	wsk = &nazwa;
+	cout << "WSK=" << wsk << endl;
+	cout << "*WSK=" << *wsk << endl;
+	cout << "Liczba=" << nazwa << endl;
+	cout << "&Liczba=" << &nazwa << endl;
+	cout << "&WSK=" << &wsk << endl;
+	return 0;
+}
---
+++ Zadanie2/Zadanie2/Zadanie2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	int tablica[5] = { 1,2,3,4,5 };
+
+	cout << "tablica = " << tablica << endl;
+	cout << "&tablica = " << &tablica << endl << endl;
+	cout << "&tablica[0] = " << &tablica[0] << endl;
+	cout << "&tablica[1] = " << &tablica[1] << endl;
+	cout << "&tablica[2] = " << &tablica[2] << endl;
+	cout << "&tablica[3] = " << &tablica[3] << endl;
+	cout << "&tablica[4] = " << &tablica[4] << endl << endl;
+
+	return 0;
+}
+
